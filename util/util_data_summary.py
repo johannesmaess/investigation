b872bcfdc56bfbf635be1a0cdcc8ab071e313df1
Johannes Maeß <maess.johannes@gmail.com>
2022-12-12 16:42:07 +0100

    LRP Trans matrices for subnetwork; better Visualizations

--- util/util_data_summary.py
+++ util/util_data_summary.py
@@ -1,6 +1,8 @@
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
+import seaborn as sns
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 def percentile(n):
     def percentile_(x):
@@ -19,8 +21,38 @@ def heatshow(data, print_data=False, lim=None, ax=plt):
     if print_data: print(data[:, (np.abs(data).sum(axis=0) != 0)].round(2))
     return ax.imshow(data, vmin=-lim, vmax=lim, cmap="RdYlGn")
 
+def plot_hists_on_ax(ax, vals_for_point, bins = [0, 1e-5, 1e-4, 1e-3, 1e-2, .1, 1, 10, 100, 1e4, 1e5, 1e6, 1e7], max_val=1e4): 
+    bins = np.array(bins)
+    if max_val: bins = bins[bins <= max_val]
+    assert len(bins.shape) == 1 and np.all(np.diff(bins) > 0), "Bin boundaries should be monotonically increasing"
+    assert not np.any(vals_for_point < bins[0]),  "Bin Range starts too high"
+    assert not np.any(vals_for_point > bins[-1]), "Bin Range ends too low"
 
-def distribution_histogram(comp_vals, bins = [0, 1e-5, 1e-4, 1e-3, 1e-2, .1, 1, 10, 100, 1e4], cutoff = 1e-2):
+    divider = make_axes_locatable(ax)
+    bins = [0] + list(bins[bins >= vals_for_point.min()])
+    x = np.arange(len(bins) - 1)
+    
+    def pretty(num):
+        if 0.01 < num <= 100: return f"{num:.3f}".rstrip('0').rstrip('.')                                       # floating
+        return f"{num:.1e}".replace('.0e', 'e').replace('e+00', '').replace('e+0', 'e+').replace('e-0', 'e-')   # scientific
+    bar_lbls = ["$ < \sigma \leq$ " + pretty(bins[i+1]) for i in range(len(bins)-1)]
+
+    for i_gamma, vals_for_gamma in enumerate(vals_for_point):
+        counts, _ = np.histogram(vals_for_gamma, bins=bins)
+        ax.bar(x, counts)
+
+        ax_title = f'Mean: {vals_for_gamma.mean():.1f}\n95th Perc: {np.percentile(vals_for_gamma, 95).round(1)}\nStdv: {vals_for_gamma.std():.1f}'
+        ax.annotate(ax_title, xy=(0.85, 0.8), xycoords='axes fraction')
+
+        # add another split to the plot (except after the last iteration):
+        if i_gamma == len(vals_for_point)-1: 
+            ax.set_xticks(x, bar_lbls)
+        else:
+            ax.set_xticks([])
+            ax = divider.append_axes("bottom", size="100%", pad=0, sharey=ax)
+
+
+def distribution_plot(vals, gammas, mode='hist', cutoff = 1e-2, aggregate_over=None, agg=False, **hist_kwargs):
     """
     Plots the distribution of Eigenvalues/Singularvalues 
         - per weight
@@ -31,74 +63,47 @@ def distribution_histogram(comp_vals, bins = [0, 1e-5, 1e-4, 1e-3, 1e-2, .1, 1, 
     cutoff is the lower bound for Vals to be shown. 
     The Val is ommitted for all gammas, if it is not > cutoff for at least one gamma.
     """
-    bins = np.array(bins)
-    assert len(bins.shape) == 1 and np.all(np.diff(bins) > 0), "Bin boundaries should be monotonically increasing"
-    assert not np.any(comp_vals < bins[0]), "Bin Range starts too high"
-    assert not np.any(comp_vals > bins[-1]), "Bin Range ends too low"
+    lbl_point=None
+    sharey='row'
+    if aggregate_over=='points' or agg:
+        n_trans, n_point, n_gamma, n_vals = vals.shape
+        vals = vals.transpose((0, 2, 3, 1)).reshape((n_trans, 1, n_gamma, n_point * n_vals))
+        lbl_point=f"[{0}, {n_point-1}]"
+        sharey=False
 
-    for data_for_weight in comp_vals:
-        num_points, num_gamma, _ = data_for_weight.shape
-        fig, axs = plt.subplots(num_gamma, num_points, figsize=(10*num_points, 4*num_gamma), sharey=True)
-        axs = np.array(axs).reshape((num_gamma, num_points))
+    n_trans, n_point, n_gamma, _ = vals.shape
 
-        for i_point, data_for_point in enumerate(data_for_weight):
+    # order plots horizontally. and additionally vertically, if multiple LRP-transformation & reference points  are to be passed.
+    n_ax = (1, n_trans) if n_point==1 else (n_trans, n_point)
+    figsize = [10*n_ax[1], 4*n_ax[0] * (0.9*n_gamma)**(mode=='hist')]
+
+    fig, axs = plt.subplots(*n_ax, figsize=figsize, sharey=sharey)
+    axs = np.array(axs).reshape((n_trans, n_point)) # for correct loop iteration
+
+    hist_kwargs['max_val'] = vals.max() * 10
+
+    for i_trans, vals_for_trans in enumerate(vals):
+        for i_point, vals_for_point in enumerate(vals_for_trans):
             if cutoff: # show only those Svals, that had a relevant value for gamma=0
-                mask = np.any(data_for_point > cutoff, axis=0)
-                data_for_point = data_for_point[:, mask]
-                bins = [0] + list(bins[bins >= cutoff])
-                
-            for i_gamma, data_for_gamma in enumerate(data_for_point):
-                (counts, bins) = np.histogram(data_for_gamma, bins=bins)
-                ax = axs[i_gamma, i_point]
+                mask = np.any(vals_for_point > cutoff, axis=0)
+                vals_for_point = vals_for_point[:, mask]
 
-                def pretty(num):
-                    if 0.01 < num <= 100: return f"{num:.3f}".rstrip('0').rstrip('.')                                       # floating
-                    return f"{num:.1e}".replace('.0e', 'e').replace('e+00', '').replace('e+0', 'e+').replace('e-0', 'e-')   # scientific
-                bar_lbls = ["$ < \sigma \leq$ " + pretty(bins[i+1]) for i in range(len(bins)-1)]
+            ax = axs[i_trans, i_point]
+            ax.title.set_text(f"w{i_trans} p{lbl_point or i_point}. Distribution of {vals_for_point.shape[1]} non-zero Singular values for {n_gamma} gammas.")
 
-                x = np.arange(len(counts))
-                ax.set_xticks(x, bar_lbls)
-                ax.bar(x, counts)
-                # ax.plot(x, counts)
+            if mode=='hist':       
+                plot_hists_on_ax(ax, vals_for_point, **hist_kwargs)
+            elif mode in ['box', 'violin']:
+                if mode=='box':    ax.boxplot(vals_for_point.T)
+                if mode=='violin': ax.violinplot(vals_for_point.T)
 
-                ax.title.set_text(f'Mean: {data_for_gamma.mean():.1f}, 95th Perc: {np.percentile(data_for_gamma, 95).round(1)}, Stdv: {data_for_gamma.std():.1f}')
-                
-        plt.suptitle(f"Distribution of {len(data_for_gamma)} non-zero Singular values.\nFor {len(axs[0])} data points (columns) and {len(axs)} gammas (rows).", fontsize=30)
-        plt.show()
+                ax.set_yscale('log')
+                ax.set_ylim(max(1e-9, cutoff/1.5), vals.max()*1.5)
 
+                ax.set_xlabel("$\gamma$")
+                ax.set_xticks(1+np.arange(len(gammas)), gammas)
+            else: raise f"Invalid mode: {mode}"
 
-def distribution_boxplot(comp_vals, gammas, cutoff = 1e-2):
-    """
-    Plots the distribution of Eigenvalues/Singularvalues 
-        - per weight
-        - per datapoint
-        - per gamma (visualized as multiple box's in one plot).
+    plt.subplots_adjust(wspace=0.1, hspace=0.16) 
+    plt.show()
 
-    cutoff is the lower bound for Vals to be shown. 
-    The Val is ommitted for all gammas, if it is not > cutoff for at least one gamma.
-    """
-
-    for data_for_weight in comp_vals:
-        num_points, num_gamma, _ = data_for_weight.shape
-        fig, axs = plt.subplots(1, num_points, figsize=(10*num_points, 4), sharey=True)
-        if num_points==1: axs=[axs]
-        axs[0].set_ylabel("Singular values")
-
-        for ax, data_for_point in zip(axs, data_for_weight):
-            ax.set_yscale('log')
-            ax.set_ylim(1e-9, 1000)
-
-            if cutoff is not None: # show only those Svals, that had a relevant value for gamma=0
-                mask = np.any(data_for_point > cutoff, axis=0)
-                data_for_point = data_for_point[:, mask]
-                
-                ax.set_ylim(cutoff, 1000)
-
-            # sns.violinplot(data_for_gamma, ax=ax)
-            ax.boxplot(data_for_point.T)
-            
-            ax.set_xlabel("$\gamma$")
-            ticks = [str(g) if g < 1e8 else '$\inf$' for g in gammas]
-            ax.set_xticks(np.arange(len(ticks))+1, ticks)
-                
-        plt.show()
