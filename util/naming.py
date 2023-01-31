f9f478f06ce64951820f57dd25fd4d1690307b50
Johannes Maeß <maess.johannes@gmail.com>
2023-01-31 12:53:50 +0000

    gammas40 in naming.py

--- util/naming.py
+++ util/naming.py
@@ -12,6 +12,9 @@ d3_tag = 'cb1-8-8-8_cb2-16-16-16_seed-0'
 gammas3 = [0, .25, 'inf']
 gammas5 = [0, .125, .25, .5, 'inf']
 gammas_0_1_21_inf = list(np.linspace(0,1,21).round(3)) + ['inf']
+# from 0 to 1000, roughly log distributed
+gammas40 = np.concatenate(([0, 1e-5, 3e-5, 1e-4, 3e-4], np.linspace(0.001, .01, 7)[:-1], np.linspace(0.01, .05, 7)[:-1], np.linspace(.05, .2, 7)[:-1], np.linspace(.2, 1, 7), 2**(np.arange(10)+1)))
+
 
 d3_after_conv_layer = [3, 5, 8, 10, 12, 16]
 
